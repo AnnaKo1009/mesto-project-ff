250f507b90edf345cf0a599454df1d88da8acaab
AnnaKo1009 <anna.conovalova2018@gmail.com>
2025-03-28 22:44:13 +0300

    iteration-1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Проектная работа Mesto
 
+https://github.com/AnnaKo1009/mesto-project-ff
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
-
+const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-
+function createCard (cardTemplate, cardName, cardLink, deleteCard) {
+    const cardTemplateClone = cardTemplate.querySelector('.places__item ').cloneNode(true);
+    const cardImage = cardTemplateClone.querySelector('.card__image');
+    const cardTitle = cardTemplateClone.querySelector('.card__title');
+    const deleteButton = cardTemplateClone.querySelector('.card__delete-button');
+    cardImage.src = cardLink;
+    cardImage.alt = cardName;
+    cardTitle.textContent = cardName;
+    deleteButton.addEventListener('click', () => deleteCard(cardTemplateClone));
+    return cardTemplateClone;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard (card) {
+    card.remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (item) {
+   const cardName =  item.name;
+   const cardLink = item.link;
+   const cardItem = createCard(cardTemplate, cardName, cardLink, deleteCard);
+   placesList.append(cardItem);
+});
+
